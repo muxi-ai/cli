47f41c5d7136f7e717a8e99bc69f2673269fdb5d
Ran Aroussi <ran@aroussi.com>
2025-12-12 11:57:22 +0000

    fix: remove blank line before badge

--- src/pkg/formation/flags.go
+++ src/pkg/formation/flags.go
@@ -131,7 +131,6 @@ func PrintBadge(formationID, serverName string) {
 	// Subtract 1 for the variation selector in ⚙︎ (U+FE0E)
 	visualWidth := utf8.RuneCountInString(content) - 1
 
-	fmt.Println()
 	fmt.Println(dim("  ╭" + strings.Repeat("─", visualWidth) + "╮"))
 	fmt.Println(dim("  │") + content + dim("│"))
 	fmt.Println(dim("  ╰" + strings.Repeat("─", visualWidth) + "╯"))
